d4b82d24db787293243b937197fcc03028a3f682
yabiball <yabibalhunie@gmail.com>
2025-01-12 13:31:05 +0300

    update login page

--- client/src/pages/SignIn/SignIn.tsx
+++ client/src/pages/SignIn/SignIn.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = import.meta.env.VITE_API_URL;
 
 export default function AuthPage() {
   const [isSignIn, setIsSignIn] = useState(true);
